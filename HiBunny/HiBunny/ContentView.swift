cc0dc04114e85457a8b54332c82ca61bf84efa75
Niwei Li <cecily.li.822@gmail.com>
2022-10-27 09:55:01 -0700

    create xcode project

---
+++ HiBunny/HiBunny/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HiBunny
+//
+//  Created by new on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HiBunny/HiBunny/HiBunnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiBunnyApp.swift
+//  HiBunny
+//
+//  Created by new on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HiBunnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
