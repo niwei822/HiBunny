c06c8250019168e301038ea8ebf3eaa1dc948f5d
niwei822 <cecily.li.822@gmail.com>
2022-10-27 22:36:07 -0700

    add launch screen and tabview

---
+++ HiBunny/HiBunny/Helpers/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  HiBunny
+//
+//  Created by cecily li on 10/27/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct MyTheme {
+        static var pinkColor: Color {
+            return Color("Colorpink")
+        }
+    }
+}
---
+++ HiBunny/HiBunny/Views/Screens/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HiBunny
+//
+//  Created by new on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("first screen")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            Text("second screen")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("third screen")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            Text("second screen")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+        .accentColor(.purple)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
